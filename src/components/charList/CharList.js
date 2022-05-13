11edbf29aaba84a1ce61a65789fe815d1cbe8a0a
Taras Sizonenko <prostartravelcom@gmail.com>
2022-05-13 17:23:57 +0300

    Не смог запулить

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,99 +1,142 @@
-import "./charList.scss";
 import { Component } from "react";
+import PropTypes from "prop-types";
 
-import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MarvelService from "../../services/MarvelService";
+import "./charList.scss";
 
 class CharList extends Component {
   state = {
     charList: [],
-    loading: false,
+    loading: true,
     error: false,
+    newItemLoading: false,
     offset: 210,
-    newItemsLoading: false,
     charEnded: false,
   };
 
   marvelService = new MarvelService();
 
   componentDidMount() {
-    this.newItemsLoading();
+    this.onRequest();
   }
 
-  newItemsLoading = (offset) => {
-    this.onCharLoading();
+  onRequest = (offset) => {
+    this.onCharListLoading();
     this.marvelService
       .getAllCharacters(offset)
-      .then(this.onCharLoaded)
+      .then(this.onCharListLoaded)
       .catch(this.onError);
   };
 
-  onCharLoading = () => {
-    this.setState({ newItemsLoading: true });
+  onCharListLoading = () => {
+    this.setState({
+      newItemLoading: true,
+    });
   };
-  onCharLoaded = (newCharlist) => {
+
+  onCharListLoaded = (newCharList) => {
     let ended = false;
-    if (newCharlist.length < 9) {
+    if (newCharList.length < 9) {
       ended = true;
     }
+
     this.setState(({ offset, charList }) => ({
-      charList: [...charList, ...newCharlist],
-      loading: true,
-      newItemsLoading: false,
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
       offset: offset + 9,
       charEnded: ended,
     }));
   };
 
-  getItems = (charlist) => {
-    const items = charlist.map((item) => {
-      const itemStyle =
-        item.id === this.props.selectedChar
-          ? "char__item char__item_selected"
-          : "char__item";
-      const imgStyle =
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
+
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+    // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+    // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+    // в отдельный компонент. Но кода будет больше, появится новое состояние
+    // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+    // По возможности, не злоупотребляйте рефами, только в крайних случаях
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+
+  // Этот метод создан для оптимизации,
+  // чтобы не помещать такую конструкцию в метод render
+  renderItems(arr) {
+    const items = arr.map((item, i) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
         item.thumbnail ===
         "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-          ? { objectFit: "fill" }
-          : null;
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
 
       return (
         <li
-          className={itemStyle}
+          className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
         </li>
       );
     });
-    return items;
-  };
-
-  onError = (charList) => {
-    this.setState({ error: true, loading: false });
-  };
+    // А эта конструкция вынесена для центровки спиннера/ошибки
+    return <ul className="char__grid">{items}</ul>;
+  }
 
   render() {
-    const { charList, loading, error, offset, newItemsLoading, charEnded } =
+    const { charList, loading, error, offset, newItemLoading, charEnded } =
       this.state;
 
-    const items = this.getItems(charList);
-    const spinner = !(loading || error) ? <Spinner /> : null;
+    const items = this.renderItems(charList);
+
     const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
 
     return (
-      <div className={"char__list "}>
-        {spinner}
+      <div className="char__list">
         {errorMessage}
-        <ul className="char__grid">{items}</ul>
+        {spinner}
+        {content}
         <button
           className="button button__main button__long"
-          disabled={newItemsLoading}
+          disabled={newItemLoading}
           style={{ display: charEnded ? "none" : "block" }}
-          onClick={() => this.newItemsLoading(offset)}
+          onClick={() => this.onRequest(offset)}
         >
           <div className="inner">load more</div>
         </button>
@@ -102,17 +145,8 @@ class CharList extends Component {
   }
 }
 
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired,
+};
+
 export default CharList;
-
-
-const storeName = 'prostar';
-
-const storeDescriprion={
-    budget:10000,
-    emploees:['taras','nika','alex'],
-    products:{
-        shigar: 20,
-        whater:10,
-            },
-    open:true,
-    };
